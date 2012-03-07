16c69f57a48879509f9aea2db221fb0d87770206
richardszalay <richard@richardszalay.com>
2012-03-07 17:45:01 +0000

    Started to split up settings into menu + screens

---
+++ source/RichardSzalay.PocketCiTray/View/EditNotificationSettings.xaml.cs
@@ -0,0 +1,13 @@
+﻿using RichardSzalay.PocketCiTray.Infrastructure;
+using WP7Contrib.View.Transitions.Animation;
+using System;
+namespace RichardSzalay.PocketCiTray.View
+{
+    public partial class EditNotificationSettings
+    {
+        public EditNotificationSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
